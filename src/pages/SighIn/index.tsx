1c6e846c15bde792dd5dfd06a5332be08e33075e
Rafael Vieira <rafaelleveske@gmail.com>
2020-11-27 20:40:10 -0300

    Configurando Navegação

---
+++ src/pages/SighIn/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { Container } from './styles';
+
+const SighIn: React.FC = () => {
+  return <Container />;
+};
+
+export default SighIn;
