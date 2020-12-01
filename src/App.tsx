cea4b6093dcacfa74171a5031f2a1d480062c77b
Rafael Vieira <rafaelleveske@gmail.com>
2020-12-01 10:29:20 -0300

    Autenticação de usuários

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,19 @@ import React from 'react';
 import { View, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 
+import AppProvider from './hooks';
+
 import Routes from './routes';
 
 const App: React.FC = () => {
   return (
     <NavigationContainer>
       <StatusBar barStyle="light-content" backgroundColor="#45486f" />
-      <View style={{ flex: 1, backgroundColor: '#45486f' }}>
-        <Routes />
-      </View>
+      <AppProvider>
+        <View style={{ flex: 1, backgroundColor: '#45486f' }}>
+          <Routes />
+        </View>
+      </AppProvider>
     </NavigationContainer>
   );
 };
